f25ec838f58140b653598241d569b20fffa63e09
Kevin Wang <imkevin.wang@mail.utoronto.ca>
2026-01-12 13:45:13 -0500

    add run code for attempt1

--- attempt1/test.py
+++ attempt1/test.py
@@ -1,8 +1,12 @@
+import cuda.bindings.driver as cuda
+
 from cutlass import cute
 
 from typing import Tuple
 
 from layout import *
+from cutlass.cute.runtime import from_dlpack
+import torch
 
 
 class Test:
@@ -11,6 +15,10 @@ class Test:
         self.tile_shape_mnk = (128, 256, 64)
         self.dtype = cutlass.BFloat16
         self.shared_storage = None
+    
+    def __call__(self, a: cute.Tensor, b: cute.Tensor, stream: cuda.CUstream):
+        # just launch 1 warpgroup that does these TMA loads and yeah
+        self.kernel(a, b).launch(grid=1, block=128, stream=stream)
     
     @cute.kernel
     def kernel(self, a: cute.Tensor, b: cute.Tensor):
@@ -34,3 +42,19 @@ class Test:
         cls = type("SharedStorage", (), dict())
         cls.__annotations__ = fields # Store stuff in annotations
         self.shared_storage = cute.struct(cls)
+
+def convert_from_dlpack(tensor, mode=0, stride_order=(0, 1)):
+    return from_dlpack(tensor.detach(), assumed_align=16).mark_compact_shape_dynamic(
+        mode=mode, stride_order=stride_order
+    )
+
+
+if __name__ == '__main__':
+    lst = [i for i in range(128*256)]
+    a = torch.tensor(lst, dtype=torch.bfloat16).reshape((128, 256)).to('cuda')
+    b = torch.tensor(lst, dtype=torch.bfloat16).reshape((128, 256)).to('cuda')
+    current_stream = cuda.CUstream(torch.cuda.current_stream().cuda_stream)
+    a_cute = convert_from_dlpack(a)
+    b_cute = convert_from_dlpack(b)
+    tk = Test()
+    tk(a, b, current_stream)
